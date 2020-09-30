b676ce2e6814d17ada40b582f8aa734cb862cbd2
Pavel Zhachko <rammiel@yandex.ru>
2020-09-30 12:16:07 +0300

    refactor: countCats functional

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,9 +3,12 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(backyard/* matrix */) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  let number = 0
-  if (backyard.flat().includes('^^')) {
-    number = backyard.flat().filter(el => el === '^^').length
-  }
+
+  let number = 0;
+  backyard.flat().forEach(element => {
+    if (element === '^^') {
+      number += 1;
+    }
+  });
   return number
 };
