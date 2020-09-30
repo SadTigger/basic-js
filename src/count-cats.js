cdb682449ef4ae2e48d99dfb5a15afd630d9e841
Pavel Zhachko <rammiel@yandex.ru>
2020-09-30 04:08:03 +0300

    feat: add functional to count-cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard/* matrix */) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let number = 0
+  if (backyard.flat().includes('^^')) {
+    number = backyard.flat().filter(el => el === '^^').length
+  }
+  return number
 };
