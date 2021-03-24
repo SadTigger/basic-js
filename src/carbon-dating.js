77a8783e530621533ee6ad1a218994d2b2516428
Pavel Zhachko <rammiel@yandex.ru>
2021-03-25 02:54:51 +0300

    fix: answer rounding

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,14 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof(sampleActivity) !== 'string' || sampleActivity === undefined) return false
-  if (sampleActivity === '' || isNaN(parseFloat(sampleActivity)) || parseFloat(sampleActivity) <= 0 ) return false
+  if (typeof (sampleActivity) !== 'string' || sampleActivity === undefined) return false
+  if (sampleActivity === '' || isNaN(parseFloat(sampleActivity)) || parseFloat(sampleActivity) <= 0) return false
   if (parseFloat(sampleActivity) > MODERN_ACTIVITY) return false
-  
+
   let number = 0;
-  number = Math.log( parseFloat(sampleActivity) / MODERN_ACTIVITY ) / (-0.693) * HALF_LIFE_PERIOD 
-  return number.toFixed(0)
+  number = Math.log(parseFloat(sampleActivity) / MODERN_ACTIVITY) / (-0.693) * HALF_LIFE_PERIOD
+  return Math.ceil(number)
 };
